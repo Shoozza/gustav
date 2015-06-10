50845e62c86c53385a771bbf3fc43f16473a63ea
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2015-06-10 11:18:13 +0200

    Add map loading code

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,39 +1,69 @@
 package;
 
-import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.ui.FlxButton;
-import flixel.util.FlxMath;
+import flixel.FlxG;
+import haxe.io.Path;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.tiled.TiledMap;
 
-/**
- * A FlxState which can be used for the actual gameplay.
- */
 class PlayState extends FlxState
 {
-	/**
-	 * Function that is called up when to state is created to set it up. 
-	 */
 	override public function create():Void
 	{
 		super.create();
+
+		var tiledLevel:TiledMap = new TiledMap("assets/data/hospital.tmx");
+
+		var tileSize = tiledLevel.tileWidth;
+		var mapW = tiledLevel.width;
+		var mapH = tiledLevel.height;
+
+		for (layer in tiledLevel.layers)
+		{
+			// for csv output
+			//var layerData:String = layer.csvData;
+
+			var layerData:Array<Int> = layer.tileArray;
+
+			var tilesheetName:String = layer.properties.get("tilesheet");
+			var tilesheetPath:String = "assets/images/" + tilesheetName;
+
+			var level:FlxTilemap = new FlxTilemap();
+
+			level.widthInTiles = mapW;
+			level.heightInTiles = mapH;
+
+			var tileGID:Int = getStartGid(tiledLevel, tilesheetName);
+
+			level.loadMap(layer.tileArray, tilesheetPath, tileSize, tileSize, FlxTilemap.OFF, tileGID);
+			add(level);
+		}
 	}
-	
-	/**
-	 * Function that is called when this state is destroyed - you might want to 
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
+
+	function getStartGid (tiledLevel:TiledMap, tilesheetName:String):Int
+	{
+		var tileGID:Int = 1;
+
+		for (tileset in tiledLevel.tilesets)
+		{
+			var tilesheetPath:Path = new Path(tileset.imageSource);
+			var thisTilesheetName = tilesheetPath.file + "." + tilesheetPath.ext;
+			if (thisTilesheetName == tilesheetName)
+			{
+				tileGID = tileset.firstGID;
+			}
+		}
+
+		return tileGID;
+	}
+
+	override public function update():Void
+	{
+		super.update();
+	}
+
 	override public function destroy():Void
 	{
 		super.destroy();
 	}
-
-	/**
-	 * Function that is called once every frame.
-	 */
-	override public function update():Void
-	{
-		super.update();
-	}	
 }
