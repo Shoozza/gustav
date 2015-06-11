99eabe79ee54eac54d4f2258bad5fb7eb480bf9b
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2015-06-12 00:18:48 +0200

    Add logitech gamepad input support

--- source/Player.hx
+++ source/Player.hx
@@ -2,10 +2,15 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.input.gamepad.LogitechButtonID;
 
 class Player extends FlxSprite
 {
 	private static inline var SPEED = 4;
+	private var _gamePad:FlxGamepad;
+	private var _direction:Float;
+	private var _strafe:Bool;
 
 	public function new(X, Y)
 	{
@@ -16,6 +21,50 @@ class Player extends FlxSprite
 		animation.add("down", [2]);
 		animation.add("right", [3]);
 		animation.play("up");
+
+		_gamePad = FlxG.gamepads.lastActive;
+		if (_gamePad == null)
+		{
+			trace("failed to find gamepad");
+		}
+	}
+
+	private function gamepadControls():Void
+	{
+		_strafe = _gamePad.pressed(LogitechButtonID.SEVEN);
+
+		_direction = _gamePad.getXAxis(LogitechButtonID.LEFT_ANALOGUE_X);
+		if (_direction != 0)
+		{
+			if (_direction < 0)
+			{
+				if (!_strafe)
+					animation.play("left");
+				x -= SPEED;
+			}
+			else
+			{
+				if (!_strafe)
+					animation.play("right");
+				x += SPEED;
+			}
+		}
+		_direction = _gamePad.getYAxis(LogitechButtonID.LEFT_ANALOGUE_Y);
+		if (_direction != 0)
+		{
+			if (_direction < 0)
+			{
+				if (!_strafe)
+					animation.play("up");
+				y -= SPEED;
+			}
+			else
+			{
+				if (!_strafe)
+					animation.play("down");
+				y += SPEED;
+			}
+		}
 	}
 
 	override public function update():Void
@@ -41,6 +90,11 @@ class Player extends FlxSprite
 			if (!FlxG.keys.pressed.SHIFT)
 				animation.play("down");
 			y += SPEED;
+		}
+
+		if (_gamePad != null)
+		{
+			gamepadControls();
 		}
 	}
 
